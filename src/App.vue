9d39a12e8ef8feef26820bb1150480da86f02089
maliksvd <malik.krechiche@gmail.com>
2023-10-16 18:54:37 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="flex flex-col justify-center h-screen">
+    <div class="main-card w-full">
+      <div class="left-side">
+        <h1 class="text-white font-bold text-2xl">Library</h1>
+        <p class="text-[#EBEBF599] text-sm">All music</p>
+      </div>
+      <div class="right-side">right</div>
+    </div>
+  </div>
+</template>
+
