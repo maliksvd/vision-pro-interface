f4b4d375d3c0a2b857a6765d6864efbbb780f135
maliksvd <malik.krechiche@gmail.com>
2023-10-17 19:32:18 -0400

    feat: add elements sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,38 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import IconsClock from "./components/Icons/Clock.vue";
+import IconsMicro from "./components/Icons/Micro.vue";
+import IconsFolder from "./components/Icons/Folder.vue";
+import IconsMusic from "./components/Icons/Music.vue";
+import IconsUser from "./components/Icons/User.vue";
 </script>
 
 <template>
   <div class="flex flex-col justify-center h-screen">
     <div class="main-card w-full">
       <div class="left-side">
-        <h1 class="text-white font-bold text-2xl">Library</h1>
-        <p class="text-[#EBEBF599] text-sm">All music</p>
+        <div>
+          <h1 class="text-white font-bold text-2xl">Library</h1>
+          <p class="text-[#EBEBF599] text-sm">All music</p>
+        </div>
+        <div>
+          <ul id="user-category">
+            <li>
+              <IconsClock />Recently Added
+            </li>
+            <li>
+              <IconsMicro />Artists
+            </li>
+            <li>
+              <IconsFolder />Albums
+            </li>
+            <li>
+              <IconsMusic /> Songs
+            </li>
+            <li>
+              <IconsUser /> Made for you
+            </li>
+          </ul>
+        </div>
       </div>
       <div class="right-side">right</div>
     </div>
